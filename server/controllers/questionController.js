402bc14ca5f635d788b623886ed0121fce690167
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2022-06-20 16:47:50 -0700

    initial updates
    Co-authored-by: Ju Kim <100457090+jukim98@users.noreply.github.com>
    Co-authored-by: Hannah Santoyo <98126144+hann7@users.noreply.github.com>
    Co-authored-by: Tristen Francis <96053750+TristenJF@users.noreply.github.com>
    Co-authored-by: Thomas Seo <25378216+thomasseo1@users.noreply.github.com>

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,4 +1,4 @@
-const { Question } = require('../models/questionModals.js');
+const { Question } = require('../models/questionModels.js');
 
 const questionController = {};
 
---
+++ server/routes/questionRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const questionController = require('../controller/questionController');
+
+const router = express.Router();
+
+// retrieves all questions in db
+router.get('/', questionController.getQuestions, (req, res) =>
+  res.status(200).json(res.locals.allQuestions)
+);
+
+// retrieve one question in db
+router.get('/:questionId', questionController.findQuestion, (req, res) =>
+  res.status(200).json(res.locals.question)
+);
+
+// post a question
+router.post('/', questionController.addQuestion, (req, res) =>
+  res.status(200).json('Question added')
+);
+
+// update a question
+router.patch('/', questionController.updateQuestion, (req, res) =>
+  res.status(200).json('Question updated')
+);
+
+// delete a question
+router.post('/', questionController.deleteQuestion, (req, res) =>
+  res.status(200).json('Question deleted')
+);
+
+module.exports = router;
