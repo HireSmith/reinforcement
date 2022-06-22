ddbd981b9893d1ccc17b6e2bd745a9f7c9ea5858
Ju Kim <jukim@Jus-MacBook-Pro.local>
2022-06-21 20:53:28 -0700

    Working routes minus 404 redirect on google oauth. Removed question model/controller

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 
-const questionRouter = require('./routes/questionRouter');
+// const questionRouter = require('./routes/questionRouter');
 const userRouter = require('./routes/userRouter');
 
 require('./config/passport')(passport);
@@ -20,11 +20,16 @@ app.use(express.static(path.resolve(__dirname, '../src/assets')));
 // Initialize passport session
 app.use(passport.initialize());
 
+app.get('/', function (req, res) {
+  res.send('Hello World!'); // This will serve your request to '/'.
+});
+
+
 // Not using /api anymore
 // Anything related to questions should go to /question route
-app.use('/question', questionRouter);
+// app.use('/question', questionRouter);
 // All other things (login, encryption, users) taken care of in default path
-app.use('/', userRouter);
+app.use('/api', userRouter);
 
 // Uncaught error catch-all route
 app.use((req, res) => res.status(404).send('Error 404: No content found'));
